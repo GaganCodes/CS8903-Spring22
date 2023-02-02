bdf3f5d565a7d8e60dbcfdfb78033c366fabba95
Gagandeep <76858849+GaganCodes@users.noreply.github.com>
2023-02-01 21:27:19 -0800

    Refactored script for readability.
    Moved functions to util_classes.py, leaving room for high level decisions here.

--- pytorch_script.py
+++ pytorch_script.py
@@ -3,14 +3,6 @@ Name: Gagandeep Singh Randhawa
 GTech User ID: grandhawa6
 Course: CS8903
 
-+++
-Main training and testing code for Neural Network performance
-TODO:
-    1. Refactor away the model
-    2. Refactor away the train and test functions
-    3. Find a way to store the values
-+++
-
 Code organization (separated by #++++++++++ and checkpoints):
 1. Importing libraries (non-exhaustive)
 2. Importing the dataset, separating into training and validation set
@@ -26,7 +18,11 @@ import torch
 import torch.nn as nn
 from torch.utils.data import random_split, DataLoader
 import numpy as np
-from util_classes import load_data, plot_data
+from util_classes import load_data
+from util_classes import NeuralNetwork
+from util_classes import train_loop, test_loop
+from util_classes import log_cosh_loss, mean_L1_loss, max_abs_percent_err, eval_accuracy
+from util_classes import plot_loss_data, plot_acc_data
 import time
 
 print("Checkpoint #1 - Libraries imported.")
@@ -34,8 +30,8 @@ print("Checkpoint #1 - Libraries imported.")
 # Importing the dataset and defining other global parameters
 
 t1 = time.time()
-# For readability in terminal
-line_break = "\n"+64*"+"+"\n"
+# Setting the random seed globally
+torch.manual_seed(23)
 
 # CUDA
 use_cuda = torch.cuda.is_available()
@@ -44,8 +40,12 @@ device = torch.device("cuda" if use_cuda else "cpu")
 # Hyperparameters
 learning_rate = 1e-3
 batch_size = 1024
-epochs = 200
-loss_fn = nn.L1Loss()
+epochs = 150
+loss_fn = mean_L1_loss
+acc_fn = eval_accuracy
+#metric = "Log Cosh Loss"
+metric = "Mean Abs Perct Error"
+#metric = "Max Abs Perct Err"
 
 # Importing data and splitting in train, test, validate
 all_data = load_data('all_data')
@@ -59,110 +59,38 @@ train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
 test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
 val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
 
-'''
-train_sample_batch, label_sample_batch = next(iter(train_dataloader))
-train_sample_single, label_sample_single = train_sample_batch[0], label_sample_batch[0]
-print(f"Datatype of training data: {train_sample_single.dtype}")
-'''
-
-# Tracking the MAPE
-mape_train = list()
-mape_val = list()
-
 print("Checkpoint #2 - Dataset imported.")
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Defining NN model and train/test step
-
-# Defining model
-class NeuralNetwork(nn.Module):
-    def __init__(self) -> None:
-        super(NeuralNetwork, self).__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-                                            nn.Linear(4, 8),
-                                            nn.ReLU(),
-                                            nn.Linear(8, 8),
-                                            nn.ReLU(),
-                                            nn.Linear(8, 1),
-        )
-    
-    def forward(self, x):
-        # Defining forward pass for the NN
-        return self.linear_relu_stack(x)
-
 model = NeuralNetwork()
+print(model)
 
 '''
+# This was done for debugging dtype discrepancy btw NN and data
+train_sample_batch, label_sample_batch = next(iter(train_dataloader))
+train_sample_single, label_sample_single = train_sample_batch[0], label_sample_batch[0]
+print(f"Datatype of training data: {train_sample_single.dtype}")
+
 for param in model.parameters():
     print(type(param), param.dtype)
-
 '''
-# Defining training and testing steps
-def train_loop(dataloader, model, loss_fn, optimizer, record=True):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    train_loss = 0.0
-
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
-        pred = model(X)
-
-        # Normalizing predicted value and y for MAPE
-        norm_pred = torch.div(pred, torch.unsqueeze(y, 1))
-        norm_y = torch.div(torch.unsqueeze(y, 1), torch.unsqueeze(y, 1))
-        loss = loss_fn(norm_pred, norm_y)
-
-        train_loss += loss.item()
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        '''
-        if batch%1000 == 0:
-            loss, current = loss.item(), batch*len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-        '''
-    train_loss /= num_batches
-    if record:
-        mape_train.append(100*train_loss)
-
-def test_loop(dataloader, model, loss_fn, record=True):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss = 0.0
-
-    # Not keeping track of gradient because we're only testing = forward pass only
-    with torch.no_grad():
-        for X, y in dataloader:
-            pred = model(X)
-            # Normalizing predicted value and y for MAPE
-            norm_pred = torch.div(pred, torch.unsqueeze(y, 1))
-            norm_y = torch.div(torch.unsqueeze(y, 1), torch.unsqueeze(y, 1))
-            test_loss += loss_fn(norm_pred, norm_y).item()
-    
-    test_loss /= num_batches
-    print(f"Avg loss: {(100*test_loss):>0.1f}% \n")
-
-    if record:
-        mape_val.append(100*test_loss)
 
 print("Checkpoint #3 - Model and steps defined.")
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Running optimization
 
-optimizer = torch.optim.NAdam(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.NAdam(model.parameters(), lr=learning_rate, eps=1e-07)
 
 for t in range(epochs):
-    if (t+1)%100:
-        print(f"Epoch {t+1}\n-------------------------------") 
-    train_loop(train_dataloader, model, loss_fn, optimizer)
-    test_loop(val_dataloader, model, loss_fn)
+    print(f"Epoch {t+1}\n-------------------------------") 
+    train_loop(train_dataloader, model, loss_fn, optimizer, acc_fn)
+    test_loop(val_dataloader, model, loss_fn, acc_fn)
 
-test_loop(test_dataloader, model, loss_fn, record=False)
+test_loop(test_dataloader, model, loss_fn, acc_fn, mode='test')
 
-plot_data(np.asarray(mape_train), np.asarray(mape_val), epochs)
+plot_loss_data(np.asarray(model.train_score_metric_1), np.asarray(model.val_score_metric_1), epochs, metric)
+
+plot_acc_data(np.asarray(model.train_score_metric_2), np.asarray(model.val_score_metric_2), epochs, "Accuracy")
 
 t2 = time.time()
 print(f"Runtime: {t2-t1}")
