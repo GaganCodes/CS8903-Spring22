5dd8f2eb0c449a0661881ecd71e2d01786b96139
Gagandeep <76858849+GaganCodes@users.noreply.github.com>
2023-02-09 22:07:05 -0800

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ Code organization (separated by #++++++++++ and checkpoints):
 import torch
 import numpy as np
 from data_func import load_data, split_data
-from model_func import NeuralNetwork_8_8
+from model_func import NeuralNetwork_1, NeuralNetwork_2
 from epoch_func import train_loop, test_loop, check_early_stop
-from loss_func import log_cosh_loss, eval_accuracy_max_abs_err, log_cosh_std
-from post_func import plot_loss_data, plot_acc_data, save_results_txt, plot_acc_data_logx, plot_loss_data_logx
+from loss_func import log_cosh_loss, eval_accuracy_max_abs_err
+from post_func import plot_loss_data, plot_acc_data, save_results_txt
 import time
 
 print("Checkpoint #1 - Libraries imported.")
@@ -31,7 +31,7 @@ t1 = time.time()
 # Setting the random seed globally
 torch.manual_seed(23)
 
-# CUDA
+# CUDA - NOT USING FOR NOW
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
@@ -40,7 +40,7 @@ learning_rate = 1e-3
 batch_size = 1024
 
 # Min epochs to be at least 20 for consistent early stopping
-epochs = 2000
+epochs = 150
 min_epochs = int(epochs/1)
 
 conv_criteria = 0.0001
@@ -52,9 +52,7 @@ acc_metric = "Max Abs Perct Error"
 # Importing data and splitting in train, test, validate
 all_data = load_data('all_data', device)
 train_size = int(0.70*len(all_data))
-#train_size = int(0.05*len(all_data))
 val_size = int(0.15*len(all_data))
-#val_size = int(0.01*len(all_data))
 test_size = len(all_data) - train_size - val_size
 
 train_dataloader, val_dataloader, test_dataloader = split_data(all_data, train_size, test_size, val_size, batch_size)
@@ -66,16 +64,17 @@ print(f"Val size: {len(val_dataloader.dataset)}")
 print("Checkpoint #2 - Dataset imported.")
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Defining NN model and train/test step
-model = NeuralNetwork_8_8()
+model = NeuralNetwork_2(4, 8, 8).to(device)
+print(model)
 
 '''
-# This was done for debugging dtype discrepancy btw NN and data
+# This was done for debugging dtype discrepancy btw NN and data (float32 vs float64)
 train_sample_batch, label_sample_batch = next(iter(train_dataloader))
 train_sample_single, label_sample_single = train_sample_batch[0], label_sample_batch[0]
 print(f"Datatype of training data: {train_sample_single.dtype}")
 
 for param in model.parameters():
-    print(type(param), param.dtype)
+    print(type(param), param.dtype, param.device)
 '''
 
 print("Checkpoint #3 - Model and steps defined.")
@@ -84,10 +83,22 @@ print("Checkpoint #3 - Model and steps defined.")
 
 optimizer = torch.optim.NAdam(model.parameters(), lr=learning_rate, eps=1e-07)
 
+file_log = open(f"train-mape-{model.name}.txt", "w")
+
 for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------") 
+    print(f"Epoch {t+1}\n-------------------------------")
     train_loop(train_dataloader, model, loss_fn, optimizer, acc_fn, device)
     test_loop(val_dataloader, model, loss_fn, acc_fn, device)
+
+    '''
+    # This was implemented to ensure the weights in NN are being updated
+    param = list(model.parameters())[0]
+    print(type(param), param.size())
+    print(param[0:5])
+    '''
+    
+    file_log.write(str(model.train_score_metric_2[-1]))
+    file_log.write('\n')
 
     if t+1 > min_epochs:
         if check_early_stop(model):
@@ -95,14 +106,12 @@ for t in range(epochs):
             break
         else:
             continue
-
     
-test_loop(test_dataloader, model, loss_fn, acc_fn, mode='test')
+test_loop(test_dataloader, model, loss_fn, acc_fn, device, mode='test')
 
+# Visualizing and saving relevant results
 plot_loss_data(np.asarray(model.train_score_metric_1), np.asarray(model.val_score_metric_1), loss_metric, model.name)
-
 plot_acc_data(np.asarray(model.train_score_metric_2), np.asarray(model.val_score_metric_2), acc_metric, model.name)
-
 save_results_txt(model, loss_metric, acc_metric)
 
 t2 = time.time()
