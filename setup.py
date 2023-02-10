e104181d31cf7af2fcbf1c425c23e6526b3ab548
Gagandeep <76858849+GaganCodes@users.noreply.github.com>
2023-02-09 22:17:32 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='CS8903-Spring22',
+    version='0.1',
+    platforms=['Any'],
+    license='MIT',
+    packages=[''],
+    install_requires=['matplotlib', 'numpy', 'time', 'torch'],
+)
