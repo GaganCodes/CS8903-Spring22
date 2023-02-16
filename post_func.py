0c52ee1b70f1372758fe98307c7fa46266071192
Gagandeep <76858849+GaganCodes@users.noreply.github.com>
2023-02-16 10:52:27 -0800

    Added ability to log train/test history

--- post_func.py
+++ post_func.py
@@ -135,7 +135,7 @@ def plot_acc_data_logx(train, val, acc_metric, model_name):
             fig_path = f".\\Fig\\Fig-NN-{model_name}-{acc_metric}-{fig_iter}.png"
         plt.savefig(fig_path, bbox_inches='tight', dpi=400, format="png")
 
-def save_results_txt(model, loss_metric, acc_metric, mode='val'):
+def save_results_txt(model, loss_metric=None, acc_metric=None):
     # Calculating relevant quantities
     # This case we have to store minimum value of loss
     # and minimum value of accuracy (Max abs percent error)
@@ -143,12 +143,13 @@ def save_results_txt(model, loss_metric, acc_metric, mode='val'):
     train_metric_2 = np.min(model.train_score_metric_2)
     val_metric_1 = np.min(model.val_score_metric_1)
     val_metric_2 = np.min(model.val_score_metric_2)
-    if mode == 'test':
+    if model.test_score_metric_1 and model.test_score_metric_2:
         test_metric_1 = np.min(model.test_score_metric_1)
         test_metric_2 = np.min(model.test_score_metric_2)
 
     file_iter = 0
-    file_path = f".\\Results\\Results-NN-{model.name}-{file_iter}.txt"  
+    file_path = f".\\Results\\Results-NN-{model.name}-{file_iter}.txt"
+    log_file_path = f".\\Results\\MAPE Log-NN-{model.name}-{file_iter}.txt"
 
     if not os.path.isfile(file_path):
         # Writing these quantities to the file
@@ -156,34 +157,62 @@ def save_results_txt(model, loss_metric, acc_metric, mode='val'):
         output_file.write(model.name + '\n')
         output_file.write(f"Min train {loss_metric}: " + str(train_metric_1) + "\n")
         output_file.write(f"Min val {loss_metric}: " + str(val_metric_1) + "\n")
-        if mode == 'test':
+        if model.test_score_metric_1:
             output_file.write(f"Min test {loss_metric}: " + str(test_metric_1) + "\n")
         output_file.write(32*"+"+"\n")
         output_file.write(f"Min train {acc_metric}: " + str(train_metric_2) + "\n")
         output_file.write(f"Min val {acc_metric}: " + str(val_metric_2) + "\n")
-        if mode == 'test':
+        if model.test_score_metric_2:
             output_file.write(f"Min test {acc_metric}: " + str(test_metric_2) + "\n")
         output_file.write(32*"+"+"\n")
         output_file.close()
+
+        # Writing training and validation history
+        if acc_metric:
+            output_file = open(log_file_path, 'w')
+            output_file.write(model.name + '\n')
+            output_file.write("Train Log: \n")
+            for acc in model.train_score_metric_2:
+                output_file.write(str(acc)+'\n')
+            output_file.write('\n')
+            output_file.write(32*"+"+"\n")
+            output_file.write("Validation Log: \n")
+            for acc in model.val_score_metric_2:
+                output_file.write(str(acc)+'\n')
+            output_file.close()
     else:
         file_iter += 1
         file_path = f".\\Results\\Results-NN-{model.name}-{file_iter}.txt"  
+        log_file_path = f".\\Results\\MAPE Log-NN-{model.name}-{file_iter}.txt"
         while os.path.isfile(file_path):
             file_iter += 1
             file_path = f".\\Results\\Results-NN-{model.name}-{file_iter}.txt"  
+            log_file_path = f".\\Results\\MAPE Log-NN-{model.name}-{file_iter}.txt"
         # Writing these quantities to the file
         output_file = open(file_path, 'w')
         output_file.write(model.name + '\n')
         output_file.write(f"Min train {loss_metric}: " + str(train_metric_1) + "\n")
         output_file.write(f"Min val {loss_metric}: " + str(val_metric_1) + "\n")
-        if mode == 'test':
+        if model.test_score_metric_1:
             output_file.write(f"Min test {loss_metric}: " + str(test_metric_1) + "\n")
         output_file.write(32*"+"+"\n")
         output_file.write(f"Min train {acc_metric}: " + str(train_metric_2) + "\n")
         output_file.write(f"Min val {acc_metric}: " + str(val_metric_2) + "\n")
-        if mode == 'test':
+        if model.test_score_metric_2:
             output_file.write(f"Min test {acc_metric}: " + str(test_metric_2) + "\n")
         output_file.write(32*"+"+"\n")
         output_file.close()
 
-
+        # Writing training and validation history
+        if acc_metric:
+            output_file = open(log_file_path, 'w')
+            output_file.write(model.name + '\n')
+            output_file.write("Train Log: \n")
+            for acc in model.train_score_metric_2:
+                output_file.write(str(acc)+'\n')
+            output_file.write('\n')
+            output_file.write(32*"+"+"\n")
+            output_file.write("Validation Log: \n")
+            for acc in model.val_score_metric_2:
+                output_file.write(str(acc)+'\n')
+            output_file.close()
