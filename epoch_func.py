590deeb288783308811f4a0d643c6abedb08f2df
Gagandeep <76858849+GaganCodes@users.noreply.github.com>
2023-02-20 21:09:22 -0800

    Added documentation, information shown during run

--- epoch_func.py
+++ epoch_func.py
@@ -1,31 +1,41 @@
 """
-Add description of this file
+Author: Gagandeep Randhawa
+GaTech ID: grandhawa6
+Course: CS8903
+
+BSD 3-Clause License
+Copyright (c) 2023, Gagandeep Randhawa
+All rights reserved.
+
+https://github.com/GaganCodes/CS8903-Spring22/blob/main/LICENSE
+
+================
+File with functions/classes relevant to epoch-specific steps, example: training, testing, early-stopping, etc.
+================
 """
 
 import torch
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Classes/functions specific to epoch/prediction steps
-class ValueMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
-def train_loop(dataloader, model, loss_fn, optimizer, acc_fn=None, record=True, device='cpu'):
+def train_loop(dataloader, model, loss_fn, optimizer, acc_fn=None, device='cpu', record=True):
+    """
+    Training step.
+    Ref:
+        1. https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
+    ================
+    Parameters:
+    dataloader: Dataloader object, dataset used to train the model
+    model: torch.nn object, neural network
+    loss_fn: Custom (or standard) torch loss object
+    optimizer: torch.optim object, algorithm used for training the network
+    acc_fn: Custom (or standard) torch accuracy object
+    device: String, cpu or cuda
+    record: Boolean, for storing the log of metrics
+    ================
+    Returns:
+    Nothing, but updates the model and the optimizer
+    """
     num_batches = len(dataloader)
     train_loss = 0.0
     accuracy = 0.0
@@ -49,13 +59,35 @@ def train_loop(dataloader, model, loss_fn, optimizer, acc_fn=None, record=True, 
         loss.backward()
         optimizer.step()
 
+        if batch % 100 == 0:
+            print(f"Train Batch: [{batch}/{num_batches}]\tLoss: {(train_loss/num_batches):>8f}\tAcc: {(100*accuracy):>0.1f}%")
+
+
     train_loss /= num_batches
     if record:
         model.train_score_metric_1.append(train_loss)
         if acc_fn:
             model.train_score_metric_2.append(100*accuracy)
 
-def test_loop(dataloader, model, loss_fn, acc_fn=None, record=True, mode='val', device='cpu'):
+
+def test_loop(dataloader, model, loss_fn, acc_fn=None, device='cpu', record=True, mode='val'):
+    """
+    Training step.
+    Ref:
+        1. https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
+    ================
+    Parameters:
+    dataloader: Dataloader object, dataset used to train the model
+    model: torch.nn object, neural network
+    loss_fn: Custom (or standard) torch loss object
+    acc_fn: Custom (or standard) torch accuracy object
+    device: String, cpu or cuda
+    record: Boolean, for storing the log of metrics
+    mode: String, either val or test
+    ================
+    Returns:
+    Nothing, but updates the model and the optimizer
+    """
     num_batches = len(dataloader)
     test_loss = 0.0
     accuracy = 0.0
@@ -88,6 +120,7 @@ def test_loop(dataloader, model, loss_fn, acc_fn=None, record=True, mode='val', 
         if acc_fn:
             model.test_score_metric_2.append(100*accuracy)
 
+
 def check_conv(model, conv_criteria=0.001):
     # For stopping the epochs if loss isn't reducing by defined convergence criteria
     # Kept to 0.1% by default
@@ -97,8 +130,10 @@ def check_conv(model, conv_criteria=0.001):
 
     return abs((loss_current-loss_prev)/loss_prev) <= conv_criteria
 
+
 def check_early_stop(model):
     # For stopping the epochs if validation loss is increasing
+    # Requires minimum 15-20 runs for robustness
     # For this implementation looking over 10 iterations
     loss_current = model.val_score_metric_1[-1]
     loss_prev = model.val_score_metric_1[-10]
